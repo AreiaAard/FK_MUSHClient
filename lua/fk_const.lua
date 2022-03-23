921ce3f396fee5f1b18f5a101b0dec5cdef42d32
AreiaAard <areiafk@gmail.com>
2022-03-23 16:36:34 -0400

    Add fk_const with some basic event types

---
+++ lua/fk_const.lua
@@ -0,0 +1,36 @@
+local const = {}
+
+
+-- To be used by event trackers' BroadcastPlugin/OnPluginBroadcast
+-- functions as the numeric message type.
+const.EVENT_TYPE = {
+    -- eg, standing, sitting, meditating...
+    ["POSITION"] = 1,
+
+    ["COMBAT"] = 2,
+
+    ["COMMUNICATION"] = 3,
+
+    -- eg, walking, running...
+    ["SPEED"] = 4,
+
+    -- Regaining spells, daily abilities, etc.
+    ["RECOVERY"] = 5,
+
+    -- eg, wearing eq, putting items into containers.
+    ["INVENTORY"] = 6,
+
+    -- Skill gains and level-ups.
+    ["IMPROVEMENT"] = 7,
+
+    -- Invitations, followers, etc.
+    ["GROUP"] = 8,
+
+    -- Hunger, thirst, drunkenness.
+    ["HUNGER"] = 9,
+
+    ["WEATHER"] = 10,
+}
+
+
+return const
