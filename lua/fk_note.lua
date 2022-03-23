88330ed706d41bd0c6b3dda844615d5096d1b2b5
AreiaAard <areiafk@gmail.com>
2022-03-23 16:10:00 -0400

    Localize fk_colors requirement to fk_note

--- lua/fk_note.lua
+++ lua/fk_note.lua
@@ -1,7 +1,7 @@
-fkColors = require("fk_colors")
+local fkColors = require("fk_colors")
 
 
 function fk_note(str)
-    local colorStr = string.format("{70}%s{70}", str)
+    local colorStr = string.format("{70}%s{70}\n", str)
     AnsiNote(fkColors.to_ANSI(colorStr))
 end
